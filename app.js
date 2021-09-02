97cfa7642dbbda782762da7c7bea9bad5c17b946
yashi27-27 <yashi.khandelwal27@gmail.com>
2021-09-02 20:07:34 +0530

    Data types

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-console.log('hello')
-console.log({name:"Yashi"})
-console.warn("This is a warning")
-console.error("this is error")
+// Data Types
+// primitive or non primitive 
+var Name = "Yashi"
+Name = ""
+console.log(Name)
+
+let age = 19
+console.log(age)
+
+console.log(typeof age)
+
+let Xy = 'afjdskljffd;@'
+console.log(typeof Name)
+
+let brand
+console.log(brand)
+
+//Non primitves 
+
+let arr = [1,2,3,4,5]
+arr.push("yashi")
+console.log(arr)
+console.log( typeof arr)
