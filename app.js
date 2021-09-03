ca1cc4ad45a10e00ad0fff53e30ea09a11d21107
yashi27-27 <yashi.khandelwal27@gmail.com>
2021-09-03 20:04:18 +0530

    Comparison operators

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-// Assignment operators
-let name = 'Yashi'
-let a = 9
-a+=9
-a=a+9
-console.log(a)
-console.log(name)
-a+=name
-console.log(typeof a)
-//more of 
-//a+=
-//a*=
-//a**=
+//comparator operators
+const a =21
+const b = 7
+const c = 21
+
+console.log(a>b)
+console.log(a<b)
+console.log(a==b)
+console.log(a==c)
+console.log(a>=c)
+//== 
+const e = 11
+const d = '11'
+console.log(e==d)
+
+//=== compares first by == then compare types by and
+
+const g =32
+const f='32'
+
+console.log(g===f)
+
